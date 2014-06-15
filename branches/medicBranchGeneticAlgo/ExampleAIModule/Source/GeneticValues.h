ca60cd48f2c1b9007e6554000247d6a665a29f47
hectorxxx@gmail.com <hectorxxx@gmail.com@7bf4bdb2-7088-541f-8310-1256307f7aa9>
2014-06-15 02:36:07 +0000

    Create GeneticValues.cpp class to read the genetic algorithm parameters. TODO: Define that every agent class use this method to define their own parameters.

---
+++ branches/medicBranchGeneticAlgo/ExampleAIModule/Source/GeneticValues.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <BWAPI.h>
+#include <map>
+#include <string>
+
+using namespace BWAPI;
+
+class GeneticValues {
+
+public:
+	static double getValue(int key);
+
+private:
+
+};
