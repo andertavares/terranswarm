ac7fcdb868d5bdaa5872c141196e33d25fc36cc2
Anderson <andertavares@users.noreply.github.com>
2014-06-30 21:55:18 +0000

    fixed score retrieval

--- ExampleAIModule/Source/ExampleAIModule.cpp
+++ ExampleAIModule/Source/ExampleAIModule.cpp
@@ -155,8 +155,8 @@ void ExampleAIModule::onEnd(bool isWinner) {
 	Broodwar->enableFlag(Flag::CompleteMapInformation);
 	resultFile.open ("results.txt", std::ios_base::app);
 	resultFile << "RM," << Broodwar->mapName() << "," << Broodwar->elapsedTime() << "," << gameResult << ",";
-	resultFile << me->getUnitScore() << "," << me->getBuildingScore() << "," << me->gatheredMinerals() + me->gatheredGas() << ",";
-	resultFile << enemy->getRace().getName() << "," << enemy->getUnitScore() << "," << enemy->getBuildingScore() << "," << enemy->gatheredMinerals() + enemy->gatheredGas() << endl;
+	resultFile << me->getUnitScore() + me->getKillScore() << "," << me->getBuildingScore() + me->getRazingScore() << "," << me->gatheredMinerals() + me->gatheredGas() << ",";
+	resultFile << enemy->getRace().getName() << "," << enemy->getUnitScore() + enemy->getKillScore() << "," << enemy->getBuildingScore() + enemy->getRazingScore() << "," << enemy->gatheredMinerals() + enemy->gatheredGas() << endl;
 	resultFile.close();
 	Broodwar << "Results written" << endl;
 }
