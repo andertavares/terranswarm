6e704f78ecafe0d6f44c239fbf4bcdfa6a3d8607
andersonrochatavares@gmail.com <andersonrochatavares@gmail.com@7bf4bdb2-7088-541f-8310-1256307f7aa9>
2014-07-01 02:38:28 +0000

    FITNESS HAS CHANGED! Now it is the ratio: myScore / enemyScore. Also, GAMedic writes a .xml file for every game with more detailed statistics.

--- branches/medicBranchGeneticAlgo/ExampleAIModule/Source/Parameters.h
+++ branches/medicBranchGeneticAlgo/ExampleAIModule/Source/Parameters.h
@@ -39,16 +39,3 @@ public:
 	Parameters(void);
 	~Parameters(void);
 };
-
-/*'s_gather_minerals', 's_build_barracks_denominator', 's_build_cmd_center',
-        's_attack_near', 's_attack_mid', 's_attack_far', 's_train_scv_denominator',
-        's_train_medic_ratio',
-        's_train_marine', 'k_scv_gather_minerals', 'k_scv_build_barracks', 'k_scv_build_supply',
-        'k_scv_build_supply', 'k_scv_build_cmd_center', 'k_scv_repair_near', 'k_scv_repair_mid',
-        'k_scv_repair_far', 'k_scv_explore', 'k_scv_attack_near', 'k_scv_repair_mid',
-        'k_scv_repair_far', 'k_scv_explore', 'k_scv_attack_near', 'k_scv_attack_mid',
-        'k_scv_attack_far', 'k_marine_explore', 'k_marine_attack_near', 'k_marine_attack_mid',
-        'k_marine_attack_far', 'k_general_train_scv_denominator', 'k_general_train_marine',
-        'k_general_train_medic_denominator',
-        'm_pack_size'
-		*/
