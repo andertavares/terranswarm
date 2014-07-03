87fbb8ee0246d604dee99f92215feea609c93835
Anderson <andertavares@users.noreply.github.com>
2014-07-03 02:51:48 +0000

    BUGFIX in plot_diversity: was not traversing the directories plotting 'draw' line in plot_fitness

--- ga_plot/plot_diversity.py
+++ ga_plot/plot_diversity.py
@@ -12,6 +12,8 @@ basedir = sys.argv[1]
 
 generations = len(glob.glob(os.path.join(basedir, '*')))
 
+print 'Found %d generations.' % generations
+
 #counts the number of equal individuals along the generations
 equal_array = []
 
@@ -19,17 +21,20 @@ for i in range(1, generations+1):
     equal_count = 0 #counts the number of equal individuals in this generations
 
     #retrieves all the chromosome files
-    chromo_files = glob.glob(os.path.join(basedir, 'g%d', '*.chr')) + glob.glob(os.path.join(basedir, 'g%d', '*.chr.lock'))
-
+    chromo_files = glob.glob(os.path.join(basedir, 'g%d' % i, '*.chr')) + glob.glob(os.path.join(basedir, 'g%d'  % i, '*.chr.lock'))
+    #print len(chromo_files)#, os.path.join(basedir, 'g%d', '*.chr')
     #traverse the chromosome files, making pairwise comparison for equality of content
     for c1 in range(0, len(chromo_files) - 1):
         for c2 in range(c1+1, len(chromo_files)):
             if open(chromo_files[c1]).read() == open(chromo_files[c2]).read():
                 equal_count += 1
-
+            #else:
+                #print '%s != %s' % (chromo_files[c1], chromo_files[c2])
     equal_array.append(equal_count)
 
+    print 'Generation %d analysed.' % i
+
 plt.plot(range(1, generations+1), equal_array, 'b', label='#equal')
-plt.plot(range(1, generations+1), [len(chromo_files)] * generations,'y--',label='total')
+#plt.plot(range(1, generations+1), [len(chromo_files)] * generations,'y--',label='total')
 plt.legend()
 plt.show()
