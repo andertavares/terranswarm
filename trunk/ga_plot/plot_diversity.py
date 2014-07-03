9a5760f32319977a014c5bcfb0791a6eaa14d226
andersonrochatavares@gmail.com <andersonrochatavares@gmail.com@7bf4bdb2-7088-541f-8310-1256307f7aa9>
2014-07-03 02:21:21 +0000

    created a script to plot the number of equal individuals along generations; in protoss experiments, it turned out that none is equal =/

---
+++ trunk/ga_plot/plot_diversity.py
@@ -0,0 +1,35 @@
+'''
+Plots the ammount of equal individuals along the generations
+
+'''
+import os
+import sys
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+basedir = sys.argv[1]
+
+generations = len(glob.glob(os.path.join(basedir, '*')))
+
+#counts the number of equal individuals along the generations
+equal_array = []
+
+for i in range(1, generations+1):
+    equal_count = 0 #counts the number of equal individuals in this generations
+
+    #retrieves all the chromosome files
+    chromo_files = glob.glob(os.path.join(basedir, 'g%d', '*.chr')) + glob.glob(os.path.join(basedir, 'g%d', '*.chr.lock'))
+
+    #traverse the chromosome files, making pairwise comparison for equality of content
+    for c1 in range(0, len(chromo_files) - 1):
+        for c2 in range(c1+1, len(chromo_files)):
+            if open(chromo_files[c1]).read() == open(chromo_files[c2]).read():
+                equal_count += 1
+
+    equal_array.append(equal_count)
+
+plt.plot(range(1, generations+1), equal_array, 'b', label='#equal')
+plt.plot(range(1, generations+1), [len(chromo_files)] * generations,'y--',label='total')
+plt.legend()
+plt.show()
