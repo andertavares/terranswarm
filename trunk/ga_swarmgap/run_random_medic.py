47194b3464cc355ce3a52ce6287cf55e76793782
andersonrochatavares@gmail.com <andersonrochatavares@gmail.com@7bf4bdb2-7088-541f-8310-1256307f7aa9>
2014-07-11 18:20:06 +0000

    plot_diversity adjusted to plot a diversity index instead of an similarity one; new script to run the random medic AI module for a predefined # of matches: run_random_medic

---
+++ trunk/ga_swarmgap/run_random_medic.py
@@ -0,0 +1,64 @@
+'''
+Searches the individual with the best fitness in the last generation,
+creates the c:\bestValues.txt file and run GAMedicReadValues_release.dll.
+
+'''
+import configparser
+import subprocess
+import shutil
+import paths
+import time
+import sys
+import os
+import re
+
+def go(enemy, num_matches):
+    sc_dir, cl_path = paths.read_paths()
+
+    #puts the correct .ini into bwapi.ini
+    paths.inicopy('bwapi_randomMedic_%s.ini' % enemy)
+
+    #checks if result.txt file exists:
+    results_path = os.path.join(sc_dir, 'results.txt')
+
+    if os.path.exists(results_path):
+
+        i = 1
+        while True:
+            tentative_bkp_name = 'results_old%d.txt' % i
+            if not os.path.exists(os.path.join(sc_dir, tentative_bkp_name)):
+                break
+            i += 1
+
+        print 'WARNING: a previous results.txt exists. It will be renamed to %s.\n' \
+              'A new results.txt will be created with the results will soon appear below.' % tentative_bkp_name
+        os.rename(results_path, os.path.join(sc_dir, tentative_bkp_name))
+        open(results_path, 'w').close() #create empty results.txt file
+
+    #calls chaoslauncher and monitors results.txt
+    chaosLauncher = subprocess.Popen([cl_path])
+    last_read = 0
+    while True:
+        rfile = open(results_path)
+        res_lines = rfile.readlines()
+
+        if len(res_lines) > last_read:
+            print 'Result for match %d: %s' % (len(res_lines), res_lines[-1].strip())
+            last_read = len(res_lines)
+
+        if len(res_lines) >= num_matches:
+            break
+        time.sleep(1)
+    #terminates chaoslauncher
+    #  and starcraft
+    subprocess.call("taskkill /IM starcraft.exe")
+    chaosLauncher.terminate()
+
+    print 'Matches finished. Check the results in %s file.' % results_path
+
+
+if __name__ == '__main__':
+    enemy = sys.argv[1] if len(sys.argv) > 1 else 'protoss'
+    num_matches = sys.argv[2] if len(sys.argv) > 2 else 30
+
+    go(enemy, int(num_matches))
