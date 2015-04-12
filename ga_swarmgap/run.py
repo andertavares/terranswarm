5f680335f52811ff8a409112c28d2430d48053aa
Anderson Tavares <andersonrochatavares@gmail.com>
2015-04-12 16:07:36 -0300

    checking whether necessary files are in place before experiment starts

--- ga_swarmgap/run.py
+++ ga_swarmgap/run.py
@@ -4,6 +4,8 @@ Runs the program
 '''
 import os
 import sys
+import paths
+import shutil
 import ga_manager as ga
 
 def runGA(config_file):
@@ -11,24 +13,31 @@ def runGA(config_file):
     ga.start(config_file)
     print ('DONE.')
 
+
+
 if __name__ == "__main__":
     #checks whether paths.ini already exists:
     if os.path.exists('paths.ini'):
-		numConfigs = len(sys.argv)
-		for n in range(1,numConfigs):
-			runGA(sys.argv[n])
+        #checks the existence of the AI module:
+        if not paths.ai_module_exists():
+            print 'AI module not found in %s' % paths.ai_module_path()
+            print 'Please, compile the AI module and place it there.'
+            print 'Exiting.'
+            exit()
+
+        #checks if map is in right place
+        paths.check_map_location()
+
+        numConfigs = len(sys.argv)
+
+        for n in range(1, numConfigs):
+            runGA(sys.argv[n])
 
     else: #creates the file and fills it with default values
         print ('The file paths.ini was not found.')
         print ('We will generate a default one.')
 
-        pfile = open('paths.ini','w')
-
-        pfile.write(';please check whether the starcraft directory and path to chaoslauncher match the ones in your system\n')
-        pfile.write('starcraft_dir = "E:\Starcraft"\n')
-        pfile.write('chaoslauncher_path = "C:\Program Files\BWAPI\Chaoslauncher\Chaoslauncher.exe"\n')
-
-        pfile.close()
+        paths.ini_create()
 
         print ('Done. Please check in paths.ini whether the starcraft directory and path to \n' \
               'chaoslauncher match the ones in your system. Correct them if necessary and run\n' \
