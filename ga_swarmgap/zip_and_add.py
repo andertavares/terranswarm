27785299436ab9392f08fd85d534030ed33d71d0
Anderson Tavares <andersonrochatavares@gmail.com>
2015-04-16 15:22:11 -0300

    Using argparse in zip_and_add

--- ga_swarmgap/zip_and_add.py
+++ ga_swarmgap/zip_and_add.py
@@ -2,9 +2,10 @@ import os
 import paths
 import sys
 import shutil
-import zipfile
+import argparse
 import subprocess
 import configparser
+
 
 def zip_and_add(config_file, commit_mode, no_pull=False, no_add=False, no_commit=False):
     """
@@ -19,19 +20,13 @@ def zip_and_add(config_file, commit_mode, no_pull=False, no_add=False, no_commit
     config = configparser.ConfigParser(config_file)
 
     exp_out = paths.experiment_output_path(config)
-    #print config.output_dir
 
-    ### CODIGO PRA CRIAR O ARQUIVO .ZIP ###
+    # zips the output_dir
     zip_path = '%s.zip' % exp_out
-    #zip_file = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
-    #TODO: <<<adicionar os arquivos no zip_file
-    ##zip_file.write(exp_out + "\\results.txt")
 
     shutil.make_archive(exp_out, "zip", exp_out)
     
-   # zip_file.close()
-
-   #copies zip_file to results/
+    # copies zip_file to results/
     try:
         copied_zip_path = os.path.join('results', '%s.zip' % config.output_dir)
         shutil.copyfile(zip_path, copied_zip_path)
@@ -40,42 +35,59 @@ def zip_and_add(config_file, commit_mode, no_pull=False, no_add=False, no_commit
               'to %s' % (zip_path, copied_zip_path)
         raise e
 
-    if (commit_mode == 'git'):
-        #performs a git pull
+    if commit_mode == 'git':
+        # performs a git pull
         if not no_pull:
             subprocess.call(['git', 'pull', 'origin', 'master'])
-            print 'pull attempted'
+            print 'git pull attempted'
 
-        #performs a git add
+        # performs a git add
         if not no_add:
             subprocess.call(['git', 'add', copied_zip_path])
             print 'Attemped to add files to git'
 
-            #performs a git commit
+            # performs a git commit
             if not no_commit:
                 subprocess.call(['git', 'commit', '-m', 'Result file %s' % copied_zip_path])
                 print 'git commit attempted, you must push manually'
 
-    elif (commit_mode == 'svn'):
-        #performs svn up
+    elif commit_mode == 'svn':
+        # performs svn up
         if not no_pull:
             subprocess.call(['svn', 'up'])
-            print 'up attempted'
+            print 'svn up attempted'
 
-        #performs a git add
+        # performs a svn add
         if not no_add:
             subprocess.call(['svn', 'add', copied_zip_path])
             print 'Attemped to add files to svn'
 
-        #performs a git commit
-        if not no_commit:
-            subprocess.call(['svn', 'commit', '-m', 'Result file %s' % copied_zip_path])
-            print 'svn commit attempted'
+            # performs a svn commit (only if add was performed before)
+            if not no_commit:
+                subprocess.call(['svn', 'commit', '-m', 'Result file %s' % copied_zip_path])
+                print 'svn commit attempted'
 
 if __name__ == "__main__":
-   
-    num_configs = len(sys.argv)
-    commit_mode = sys.argv[1]    
 
-    for n in range(2,num_configs):
-        zip_and_add(sys.argv[n], commit_mode, False, False, False)
+    parser = argparse.ArgumentParser(description='Adds the results of an experiment to a .zip file '
+                                                 'And commits it to git repository.')
+
+    parser.add_argument('config', metavar='num_configs', type=str, nargs='*',
+                       help='Path to a config file of an experiment to be zipped and added.')
+
+    parser.add_argument('--no-pull', action='store_true', default=False,
+                       help="Does not execute pull before the add/commit")
+
+    parser.add_argument('--no-add', action='store_true', default=False,
+                       help='Does not execute add (only zip the file)')
+
+    parser.add_argument('--no-commit', action='store_true', default=False,
+                       help='Does not perform a commit (automatically activated with --no-add).')
+
+    parser.add_argument('-m', '--commit-mode', type=str, default='git',
+                       help='Commit mode (use svn or git commands), default=git')
+
+    args = parser.parse_args()
+   
+    for cfg in args.config:
+        zip_and_add(cfg, args.commit_mode, args.no_pull, args.no_add, args.no_commit)
