4cd5a9e0ef70b3c69b0ac12ac455f7fd6fe62983
h3ct0r <h3ct0r.ml@gmail.com>
2015-05-08 17:02:35 -0300

    Added starcraft experiment server

---
+++ starcraft_ga_server/web2py/gluon/tests/test_dal.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Unit tests for gluon.dal
+"""
+
+import unittest
+from fix_path import fix_sys_path
+
+fix_sys_path(__file__)
+
+
+from gluon.dal import DAL, Field
+
+
+class TestDALSubclass(unittest.TestCase):
+    def testRun(self):
+        from gluon.serializers import custom_json, xml
+        from gluon import sqlhtml
+        db = DAL(check_reserved=['all'])
+        self.assertEqual(db.serializers['json'], custom_json)
+        self.assertEqual(db.serializers['xml'], xml)
+        self.assertEqual(db.representers['rows_render'], sqlhtml.represent)
+        self.assertEqual(db.representers['rows_xml'], sqlhtml.SQLTABLE)
+
+    def testSerialization(self):
+        import pickle
+        db = DAL(check_reserved=['all'])
+        db.define_table('t_a', Field('f_a'))
+        db.t_a.insert(f_a='test')
+        a = db(db.t_a.id>0).select(cacheable=True)
+        s = pickle.dumps(a)
+        b = pickle.loads(s)
+        self.assertEqual(a.db, b.db)
+
+""" TODO:
+class TestDefaultValidators(unittest.TestCase):
+    def testRun(self):
+        pass
+"""
